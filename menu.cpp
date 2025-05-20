198de7930ab6432835b91ba2266e74f74c0ab4d3
Zaidan221 <zaidansaputra216@gmail.com>
2025-05-20 11:21:12 +0800

    modifikasi menu utama, menambahkan menu input

--- menu.cpp
+++ menu.cpp
@@ -11,9 +11,19 @@ cout<<"2. Menampikan data"<<"\n";
 cout<<"3. Sorting"<<"\n";           
 cout<<"4. Sepatah kata"<<"\n";            
 cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
+cout<<"Masukan angka :";   
 
 }
+void input (int data[]){
+  system("cls");
+  cout << "Masukan 5 angka: "; 
+  for(int i = 0; i<5; i++){
+  cin >> data[i];
+}
+cout << "Angka telah disimpan";
+getch();
+}
+
 
 void mPertama(string pesan){
 system("cls");
@@ -32,12 +42,13 @@ char pl;
 do
 {
     dMenu();
+    int data[5];
     pl=getch();
   switch (pl)
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    input(data);
     break;
    case '2':
     mPertama("ke- dua");
